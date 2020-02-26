cabc0ad9ac4f5e1d6e9c27012497c302d0279ebc
Scott Gardner <scott.gardner@mac.com>
2020-02-26 08:27:46 -0600

    Initial Commit

---
+++ Crosstalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crosstalk
+//
+//  Created by Scott Gardner on 2/26/20.
+//  Copyright © 2020 Scott Gardner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
